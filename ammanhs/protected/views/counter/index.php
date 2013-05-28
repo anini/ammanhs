ef343c6cda51d3efe2d0baec3a5bc5bc16ccbaee
anini <mohd.anini@gmail.com>
2013-05-28 03:38:26 +0300

    Adding countdown

---
+++ ammanhs/protected/views/counter/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html> 
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=8"> 
+    <title>Amman Hackerspace | فضاء المتمكنين - عمّان</title>
+    <link rel="stylesheet" href="/protected/views/counter/js/style.css" media="all"> 
+    <script src="js/jquery.min.js" type="text/javascript"></script> 
+    <!--[if lte IE 7]>
+    <script type="text/javascript" src="../counter/js/degrade.js"></script>
+    <![endif]--> 
+  </head>
+  <body id="index">
+    <div id="wrapper">
+      <div id="container"> 
+        <div style="height: 235px;"></div>
+        <img src="/protected/views/counter/logo.png" style=" height: 550px; margin-top: -260px; position: absolute; left: 22%;">
+        <div id="canvas-content"> 
+          <div id="countdown"> 
+            <script type="text/javascript"> 
+              document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+            </script> 
+          </div> 
+          <script type="text/javascript" src="/protected/views/counter/js/base.js"></script> 
+          <script type="text/javascript" src="/protected/views/counter/js/box2d.js"></script> 
+          <script type="text/javascript" src="/protected/views/counter/js/io.js"></script> 
+        </div> 
+      </div>
+    </div>
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-40309445-1', 'ammanhs.com');
+    ga('send', 'pageview');
+    </script>
+  </body>
+</html>
