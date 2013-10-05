dcee8c0031a8b64abaa41c0022faf89c1fe4bff7
anini <mohd.anini@gmail.com>
2013-10-05 22:56:56 +0300

    Changes on the structure of the cloned photos

--- ammanhs/protected/components/ImageValidator.php
+++ ammanhs/protected/components/ImageValidator.php
@@ -14,7 +14,7 @@ class ImageValidator extends CValidator
     	}
 
     	if(!$this->sub_dir){
-    		$this->sub_dir=get_class($model);
+    		$this->sub_dir=strtolower(get_class($model));
     	}
 
     	if(property_exists(get_class($model), '_upload_index') && $model->_upload_index>=0){
@@ -27,7 +27,8 @@ class ImageValidator extends CValidator
 
         if(null!==$uploader && $uploader instanceof CUploadedFile){
         	$timestamp  =hexdec('0x'.substr(uniqid(), -8));
-        	$sub_dir    =$this->sub_dir.'/'.($timestamp%100).'/';
+        	//$sub_dir    =$this->sub_dir.'/'.($timestamp%100).'/';
+            $sub_dir    =$this->sub_dir.'/'.$model->id.'/';
         	$file_name  =Text::slugify($attr_val.'-'.$timestamp).'.'.$uploader->getExtensionName();
         	$images_path=Yii::app()->getBasePath().DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR;
 			@mkdir($images_path.$sub_dir, 0777, true);
@@ -43,15 +44,16 @@ class ImageValidator extends CValidator
     /**
      * @author  Mohammad Anini
      * @param   string $sub_directory:      Where to save the file
-     * @param   int $id:                    ID of the activity
+     * @param   int $activity_id:           ID of the activity
      * @param   string $extension:          Extension of the output file
      * @return  string Sub/directoy/file_name.ext
      */
-    public static function generateFilename($sub_directory='Threads', $id=0, $extension='jpg')
-    {           
+    public static function generateFilename($sub_directory='thread', $object_id=0, $extension='jpg')
+    {
         $timestamp=hexdec('0x'.substr(uniqid(),-8));
-        $sub_directory=$sub_directory.'/'.($timestamp%100).'/';
-        $file_name=Text::slugify($id.'-'.$timestamp).'.'.$extension;
+        //$sub_directory=$sub_directory.'/'.($timestamp%100).'/';
+        $sub_directory=$sub_directory.'/'.$object_id.'/';
+        $file_name=Text::slugify($object_id.'-'.$timestamp).'.'.$extension;
         return $sub_directory.$file_name;
     }
 }
