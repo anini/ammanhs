1b5895f0282289443e99c89d3722abfa6fcf8b30
anini <mohd.anini@gmail.com>
2013-10-11 23:59:01 +0300

    Add nofollow to external links

--- ammanhs/protected/views/layouts/_footer.php
+++ ammanhs/protected/views/layouts/_footer.php
@@ -13,23 +13,23 @@
 		<div>
 			<a onclick="open_contact_modal();"><?php echo Yii::t('core', 'Contact Us'); ?></a>
 		</div>
-		|
+		<!--|
 		<div>
-			<a target="_blank" href="http://eshtre.com/?ref=ammanhs"><?php echo Yii::t('core', 'E-Store'); ?></a>
+			<a target="_blank" rel="nofollow" href="http://eshtre.com/?ref=ammanhs"><?php echo Yii::t('core', 'E-Store'); ?></a>
 		</div>
-
+		-->
 		<div id="follow-us-footer">
 		    <div class="social-buttons-title"><?php echo Yii::t('core', 'Follow us:'); ?></div>
 		    <div class="social-buttons">
-		        <a class="facebook-button" target="_blank" href="http://facebook.com/ammanhackerspace"></a>
-		        <a class="twitter-button" target="_blank" href="http://twitter.com/ammanhs"></a>
-		        <a class="google-button" target="_blank" href="https://plus.google.com/105524057604428756913/"></a>
+		        <a class="facebook-button" target="_blank" rel="nofollow" href="http://facebook.com/ammanhackerspace"></a>
+		        <a class="twitter-button" target="_blank" rel="nofollow" href="http://twitter.com/ammanhs"></a>
+		        <a class="google-button" target="_blank" rel="nofollow" href="https://plus.google.com/105524057604428756913/"></a>
 		    </div>
 		</div>
 	</div>
 	<div id="sponsers">
-		<a id="sponser-eshtre" target="_blank" href="http://eshtre.com/?ref=ammanhs"></a>
-		<a id="sponser-jubilee" target="_blank" href="http://www.jubilee.edu.jo/"></a>
+		<a id="sponser-eshtre" target="_blank" rel="nofollow" href="http://eshtre.com/?ref=ammanhs"></a>
+		<a id="sponser-jubilee" target="_blank" rel="nofollow" href="http://www.jubilee.edu.jo/"></a>
 		<!--<a id="sponser-sparkfun" target="_blank" href="http://www.sparkfun.com/"></a>-->
 	</div>
 	<div id="footer-bg"></div>
--- ammanhs/protected/views/layouts/_sidebar.php
+++ ammanhs/protected/views/layouts/_sidebar.php
@@ -28,8 +28,8 @@
 <div id="follow-us">
     <div class="ge-ss social-buttons-title"><?php echo Yii::t('core', 'Follow us:'); ?></div>
     <div class="social-buttons">
-        <a class="facebook-button" target="_blank" href="http://facebook.com/ammanhackerspace"></a>
-        <a class="twitter-button" target="_blank" href="http://twitter.com/ammanhs"></a>
-        <a class="google-button" target="_blank" href="https://plus.google.com/105524057604428756913/"></a>
+        <a class="facebook-button" target="_blank" rel="nofollow" href="http://facebook.com/ammanhackerspace"></a>
+        <a class="twitter-button" target="_blank" rel="nofollow" href="http://twitter.com/ammanhs"></a>
+        <a class="google-button" target="_blank" rel="nofollow" href="https://plus.google.com/105524057604428756913/"></a>
     </div>
 </div>
