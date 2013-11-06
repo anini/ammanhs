c47af158408b294432c60865899cca6a63434e9c
anini <mohd.anini@gmail.com>
2013-11-06 17:13:15 +0200

    Adding activities link to the header

--- ammanhs/protected/views/layouts/_header.php
+++ ammanhs/protected/views/layouts/_header.php
@@ -13,7 +13,7 @@
     </a>
       <form class="navbar-search" action="/search">
         <input name="q" <?php if(isset($_GET['q'])) echo 'value="'.$_GET['q'].'"'; ?> type="text" class="search-query span2" placeholder="<?php echo Yii::t('core','Search'); ?>" id="header-search" 
-        onfocus="$('#main-menu').slideUp(200);setTimeout(function(){$('#header-search').removeClass('span2');$('#header-search').addClass('span4');}, 250);" onblur="$(this).removeClass('span4');$(this).addClass('span2');setTimeout(function(){$('#main-menu').slideDown(200);}, 500);"><span class='octicons octicon-search' style='margin-right: -22px;opacity: 0.5;position: absolute;margin-top: 7px'></span>
+        onfocus="$('#main-menu').slideUp(200);setTimeout(function(){$('#header-search').removeClass('span2');$('#header-search').addClass('span4');}, 250);" onblur="$(this).removeClass('span4');$(this).addClass('span2');setTimeout(function(){$('#main-menu').slideDown(200);}, 500);"><span class='octicons octicon-search'></span>
       </form>
     
      </div>
@@ -21,7 +21,7 @@
         <ul class="nav" id="main-menu">
           <li <?php if($this->action->controller->id=='site' && $this->action->id=='index') echo 'class="active"'; ?>><a href="<?php echo Yii::app()->urlManager->createUrl('site/index'); ?>"><?php echo Yii::t('core','Home'); ?></a></li>
           <li <?php if($this->action->controller->id=='thread' && $this->action->id=='index') echo 'class="active"'; ?>><a href="<?php echo Yii::app()->urlManager->createUrl('/thread/index'); ?>"><?php echo Yii::t('core','Threads'); ?></a></li>
-          <li <?php if($this->action->controller->id=='membership') echo 'class="active"'; ?>><a href="<?php echo Yii::app()->urlManager->createUrl('/membership/index'); ?>"><?php echo Yii::t('core','Memberships'); ?></a></li>
+          <li <?php if($this->action->controller->id=='activity') echo 'class="active"'; ?>><a href="<?php echo Yii::app()->urlManager->createUrl('/activity/index'); ?>"><?php echo Yii::t('core','Activities'); ?></a></li>
           <li <?php if($this->action->controller->id=='site' && $this->action->id=='about') echo 'class="active"'; ?>><a href="<?php echo Yii::app()->urlManager->createUrl('/site/about'); ?>"><?php echo Yii::t('core','About'); ?></a></li>
         </ul>
         <?php $this->renderPartial('//layouts/_user_header'); ?>
