4dc2169db571e41ff56cb44e64fade1c54847e84
anini <mohd.anini@gmail.com>
2014-05-31 09:31:03 +0300

    New mail layout

--- ammanhs/protected/views/emailTemplates/layout.php
+++ ammanhs/protected/views/emailTemplates/layout.php
@@ -1,28 +1,141 @@
-<table align="center" cellpadding="0" cellspacing="0" style="direction:rtl;padding:auto;border:4px solid rgba(0, 0, 0, 0.247059);font-family:arial;font-size:12px" width="616">
-	<tbody>
-		<tr>
-			<td style="border-bottom:5px solid rgba(0, 0, 0, 0.247059);padding:12px;background-color:rgb(229, 229, 229)" valign="top">
-				<a href="http://ammanhs.com"><img src="<?php echo "http://".Yii::app()->params['static_host']."/images/brand_ar.png"; ?>" /></a>
-			</td>
-		</tr>
-		<tr>
-			<td valign="top">
-				<div style="padding:12px;"><?php echo $content; ?></div>
-			</td>
-		</tr>
-		<tr>
-			<td valign="middle" style="background-color:rgb(229, 229, 229);padding:0 16px;font-size:11px;">
-				<div style="height:27px;line-height:30px">
-					<div style="float:right;font-family:arial">
-						<a href="<?php echo $this->createAbsoluteUrl('site/about'); ?>" style="color:#2a8cc3;text-decoration:none"><?php echo Yii::t('core', 'About'); ?></a>
-						<span style="color:#ccc">|</span>
-						<a href="<?php echo $this->createAbsoluteUrl('site/index').'#contact'; ?>" style="color:#2a8cc3;text-decoration:none"><?php echo Yii::t('core', 'Contact Us'); ?></a>
-					</div>
-					<div style="float:left;font-family:arial">
-						<?php echo Yii::t('core', 'Copy Rights - Arabic'); ?>
-					</div>
-				</div>
-			</td>
-		</tr>
-	</tbody>
-</table>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>
+	<?php echo $subject; ?>
+	</title>
+</head>
+<body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0" style="margin: 0;padding: 0;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;background-color: #F2F2F2;height: 100% !important;width: 100% !important;">
+	<center>
+		<table align="center" border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" style="direction: rtl;border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;margin: 0;padding: 0;background-color: #F2F2F2;height: 100% !important;width: 100% !important;">
+			<tr>
+				<td align="center" valign="top" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;margin: 0;padding: 20px;border-top: 0;height: 100% !important;width: 100% !important;">
+					<!-- BEGIN TEMPLATE // -->
+					<table border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;border: 0;">
+						<tr>
+							<td align="center" valign="top" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+								<!-- BEGIN HEADER // -->
+								<table border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;background-color: #FFFFFF;border-top: 0;border-bottom: 0;">
+									<tr>
+										<td valign="top"  style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+											<table border="0" cellpadding="0" cellspacing="0" width="100%"  style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+												<tr>
+													<td valign="top" style="padding: 9px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;" >
+														<table align="left" width="100%" border="0" cellpadding="0" cellspacing="0"  style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+															
+																<tr>
+																	<td valign="top" style="padding-right: 23px;padding-left: 23px;padding-top: 15px;padding-bottom: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+																		<a href="http://ammanhs.com/?ref=mail-header">
+																			<img align="center" alt="Amman Hackerspace" src="<?php echo "http://".Yii::app()->params['static_host']."/images/ammanhs_full_logo.png"; ?>" width="536" style="max-width: 1336px;padding-bottom: 0;display: inline !important;vertical-align: bottom;border: 0;outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;" >
+																		</a>
+																	</td>
+																</tr>
+															
+														</table>
+													</td>
+												</tr>
+											</table>
+										</td>
+									</tr>
+								</table>
+								<!-- // END HEADER -->
+							</td>
+						</tr>
+						<tr>
+							<td align="center" valign="top" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+								<!-- BEGIN BODY // -->
+								<table border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;background-color: #FFFFFF;border-top: 0;border-bottom: 0;">
+									<tr>
+										<td valign="top"  style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+											<table border="0" cellpadding="0" cellspacing="0" width="100%"  style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+												<tr>
+													<td valign="top"  style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+														<table align="left" border="0" cellpadding="0" cellspacing="0" width="600"  style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+															<tr>
+																<td valign="top"  style="padding-top: 9px;padding-right: 18px;padding-bottom: 9px;padding-left: 18px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #606060;font-family: Helvetica;font-size: 15px;line-height: 150%;text-align: right;">
+																	<?php echo $content; ?>
+																</td>
+															</tr>
+														</table>
+													</td>
+												</tr>
+											</table>
+										</td>
+									</tr>
+								</table>
+								<!-- // END BODY -->
+							</td>
+						</tr>
+						<tr>
+							<td align="center" valign="top" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+								<!-- BEGIN FOOTER // -->
+								<table border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;background-color: #FFFFFF;border-top: 0;border-bottom: 0;">
+									<tr>
+										<td valign="top"  style="padding: 9px 0px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+											<table border="0" cellpadding="0" cellspacing="0" width="100%"  style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+												<tr>
+													<td align="center" valign="top" style="padding: 9px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;" >
+														<table border="0" cellpadding="0" cellspacing="0" width="100%"  style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+															<tr>
+																<td align="center" style="padding-left: 9px;padding-right: 9px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+																	<table border="0" cellpadding="0" cellspacing="0" width="100%"  style="background-color: #FAFAFA;border: 1px solid #EEEEEE;border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+																		<tr>
+																			<td align="center" valign="top" style="padding: 9px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+																				<table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+																					<tr>
+																						<td valign="top" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+																							<table align="left" border="0" cellpadding="0" cellspacing="0"  style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+																								<div style="color: #606060;font-family: Helvetica;font-size: 11px;line-height: 13.75px;">
+																									Amman Hackerspace - فضاء المتمكنين | عمّان
+																									<span style="font-weight:bold;color: #EEE;font-size: 18px;padding: 0px 8px;">|</span>
+																									<a href="http://ammanhs.com/" style="color: #1155CC;word-wrap: break-word;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;font-weight: normal;text-decoration: underline;" target="_blank">http://ammanhs.com/</a>
+																									<span style="font-weight:bold;color: #EEE;font-size: 18px;padding: 0px 8px;">|</span>
+																									<a href="mailto:info@ammanhs.com" style="color: #1155CC;word-wrap: break-word;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;font-weight: normal;text-decoration: underline;" target="_blank">info@ammanhs.com</a>
+																									<span style="font-weight:bold;color: #EEE;font-size: 18px;padding: 0px 8px;">|</span>
+																									عمّان - الأردن
+																								</div>
+																							</table>
+																						</td>
+																					</tr>
+																				</table>
+																			</td>
+																		</tr>
+																	</table>
+																</td>
+															</tr>
+														</table>
+													</td>
+												</tr>
+											</table>
+										</td>
+									</tr>
+								</table>
+								<br/>
+								<table border="0" cellpadding="0" cellspacing="0" width="100%"  style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+									<tr>
+										<td valign="top"  style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+											<table align="left" border="0" cellpadding="0" cellspacing="0" width="600"  style="border-collapse: collapse;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+												<tr>
+													<td valign="top"  style="padding-top: 9px;padding-right: 18px;padding-bottom: 9px;padding-left: 18px;mso-table-lspace: 0pt;mso-table-rspace: 0pt;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #606060;font-family: Helvetica;font-size: 11px;line-height: 125%;text-align: center;">
+														<div style="color: #606060;font-family: Helvetica;font-size: 11px;line-height: 13.75px;">
+															أنت تستقبل هذا البريد الإلكتروني لأنك مسجّل في موقع فضاء المتمكنين | عمّان - لإيقاف استقبال هذا البريد الإلكتروني <a href="<?php echo "http://".Yii::app()->params['static_host']."/user/profile/?ref=unsubscribe"; ?>">اضغط هنا</a>
+														</div>
+													</td>
+												</tr>
+											</table>
+										</td>
+									</tr>
+								</table>
+								<!-- // END FOOTER -->
+							</td>
+						</tr>
+					</table>
+					<!-- // END TEMPLATE -->
+				</td>
+			</tr>
+		</table>
+	</center>
+</body>
+</html>
