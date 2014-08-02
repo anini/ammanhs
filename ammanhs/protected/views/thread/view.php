932c8df7d56b8a9ff37d809d865383c3fda7e919
anini <mohd.anini@gmail.com>
2014-08-02 19:58:24 +0300

    Fix Twitter Share Button Count

--- ammanhs/protected/views/thread/view.php
+++ ammanhs/protected/views/thread/view.php
@@ -89,7 +89,7 @@ $this->menu=array(
 							}(document, 'script', 'facebook-jssdk'));</script>
 							<div id="share-buttons">
 								<div class="fb-share-button" data-href="<?php echo $share_link; ?>" data-type="button_count"></div>
-								<a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-url="<?php echo $share_link; ?>" data-lang="en" data-related="anywhereTheJavascriptAPI" data-text="<?php echo $model->title; ?>">Tweet</a>
+								<a href="https://twitter.com/share" data-counturl="<?php echo $model->getLink(true); ?>" class="twitter-share-button" data-size="large" data-url="<?php echo $share_link; ?>" data-lang="en" data-related="anywhereTheJavascriptAPI" data-text="<?php echo $model->title; ?>">Tweet</a>
 								<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 								<div class="g-plus" data-action="share" lang="en-US" data-annotation="bubble"  data-height="28" data-href="<?php echo $share_link; ?>"></div>
 								<script type="text/javascript">
